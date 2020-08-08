60545e25c59f926a4790d1df384f3676bbc27bc9
王寿军 <1657032364@qq.com>
2020-08-08 10:31:25 +0800

    完成了

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- 上面展示 -->
+    <div>你好</div>
+    <div class="nav-bottom">
+      <a>
+        <i class="iconfont icon-jia"></i>
+        <label>首页</label>
+      </a>
+      <a>
+        <i class="iconfont icon-jia"></i>
+        <label>首页</label>
+      </a>
+      <a>
+        <i class="iconfont icon-jia"></i>
+        <label>首页</label>
+      </a>
+      <a>
+        <i class="iconfont icon-jia"></i>
+        <label>首页</label>
+      </a>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+
+</style>
